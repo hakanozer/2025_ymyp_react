17c803729fd46678daf623a96756e121f922de68
Hakan ÖZER <hakanozer02@gmail.com>
2025-02-04 22:31:35 +0300

    Likes

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -2,16 +2,37 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { singleProduct } from '../services/productService'
 import { Product } from '../models/IProduct'
+import { getSingleLikes, isLikesControl } from '../utils/store'
 
 function ProductDetail() {
 
+  const [isLike, setIsLike] = useState(false)  
   const [bigImage, setBigImage] = useState('')  
   const [item, setItem] = useState<Product>()  
   const params = useParams()
 
+  /*
+  useEffect(() => {
+    const pid = params.pid
+    if (item && pid) {
+        console.log(isLike, pid)
+    }
+  }, [isLike])
+  */
+
+  const likesControl = () => {
+    const likesStatus = !isLike
+    setIsLike(likesStatus)
+    const pid = params.pid
+    if (item && pid) {
+        isLikesControl(pid)
+    }
+  }
+
   useEffect(() => {
       const pid = params.pid
       if (pid) {
+        setIsLike(getSingleLikes(pid))
         singleProduct(pid).then(res => {
           setItem(res.data.data)
           setBigImage(res.data.data.images[0])
@@ -19,18 +40,39 @@ function ProductDetail() {
       }
   }, [])
 
+
+
+
+
   return (
     <>
         { item && 
-            <div className='row'>
-                <div className='col-sm-6'>
-                    <h3>{item.title}</h3>
-                </div>
-                <div className='col-sm-6'>
-                    <img src={bigImage} className="img-fluid" />
-                    { item.images.map( (image, index) => 
-                        <img onClick={() => setBigImage(image) } role='button' key={index} src={image} className='img-thumbnail me-2' width={120} />
-                    )}
+            <div className='container mt-5'>
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <h3 className='text-primary'>{item.title}</h3>
+                        <p className='text-muted'>{item.description}</p>
+                        <p>
+                            <i onClick={ likesControl } role='button' className={ isLike === false ? 'bi bi-suit-heart fs-2' : 'bi bi-suit-heart-fill fs-2 text-danger' }></i>
+                        </p>
+                        <p><strong>Category:</strong> {item.category}</p>
+                        <p><strong>Price:</strong> <span className='text-success'>${item.price}</span></p>
+                        <p><strong>Discount:</strong> <span className='text-danger'>{item.discountPercentage}%</span></p>
+                        <p><strong>Rating:</strong> {item.rating}</p>
+                        <p><strong>Stock:</strong> {item.stock}</p>
+                        <p><strong>Brand:</strong> {item.brand}</p>
+                        <p><strong>SKU:</strong> {item.sku}</p>
+                        <p><strong>Minimum Order Quantity:</strong> {item.minimumOrderQuantity}</p>
+                        <p><strong>Tags:</strong> {item.tags.join(', ')}</p>
+                    </div>
+                    <div className='col-md-6'>
+                        <img src={bigImage} className="img-fluid rounded shadow-sm mb-3" />
+                        <div className='d-flex flex-wrap'>
+                            { item.images.map( (image, index) => 
+                                <img onClick={() => setBigImage(image) } role='button' key={index} src={image} className='img-thumbnail me-2 mb-2' width={120} />
+                            )}
+                        </div>
+                    </div>
                 </div>
             </div>
         }
